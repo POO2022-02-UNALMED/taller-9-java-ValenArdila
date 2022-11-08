958e2c04e4fd9b3eda1e5bcc40b548c26e37e1d1
ValenArdila <vardilaq@unal.edu.co>
2022-11-07 22:30:20 -0500

    Intento1

---
+++ src/main/java/guiSolucion/Calculadora.java
@@ -0,0 +1,24 @@
+package guiSolucion;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Calculadora extends Application {
+	public static void main(String args[]){ 
+	      launch(args); //launch
+	} 
+	
+	@Override
+	public void start(Stage window) throws Exception {
+		VBox vb = new VBox();//VBox
+		window.setTitle("Calculadora");//"Calculadora"
+		
+		vb.getChildren().add(new Calculator());
+		Scene sc = new Scene(vb, 270, 270);//vb
+		window.setScene(sc);//sc
+		window.show();//window
+	}
+
+}
